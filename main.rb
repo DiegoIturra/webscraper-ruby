0b5e84f1dba49c1be20b902878cb7b07cad69848
Diego Iturra <diturra@dparadig.com>
2023-01-19 23:19:37 -0300

    feat: get url from an image and save data in a CSV file

--- main.rb
+++ main.rb
@@ -1,12 +1,25 @@
 require 'open-uri'
 require 'nokogiri'
+require 'csv'
 
 class Scraper
+    
+    @@base_url = "https://en.wikipedia.org/wiki/Douglas_Adams"
+
     def initialize()
-        html = URI.open("https://en.wikipedia.org/wiki/Douglas_Adams") #get html file
+        html = URI.open(@@base_url) #get html file
         doc = Nokogiri::HTML(html) #parse html file
         description = doc.css("p").text.strip.split("\n")[0] #get first <p> text
-        puts description
+        url_image = doc.css("img").find{|picture| picture.attributes["alt"].value.include?("Douglas adams portrait cropped.jpg")}.attributes["src"].value
+        puts url_image
+
+        #Export data to CSV
+        data_arr = []
+        data_arr.push(description, url_image)
+
+        CSV.open('data.csv', "w") do |csv|
+            csv << data_arr
+        end
     end
 end
 
