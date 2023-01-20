ca9a89b6e7a9b86590ee2d5a1b7265e9f9041739
Diego Iturra <diturra@dparadig.com>
2023-01-19 23:02:02 -0300

    feat: add nokogiri parser and test some simple page

--- main.rb
+++ main.rb
@@ -1,8 +1,13 @@
+require 'open-uri'
+require 'nokogiri'
+
 class Scraper
     def initialize()
-        puts "new scraper"
+        html = URI.open("https://en.wikipedia.org/wiki/Douglas_Adams") #get html file
+        doc = Nokogiri::HTML(html) #parse html file
+        description = doc.css("p").text.strip.split("\n")[0] #get first <p> text
+        puts description
     end
-
 end
 
 scraper = Scraper::new
