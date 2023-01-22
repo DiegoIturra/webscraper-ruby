0bb0be69f3ae6402bc4d8aed546fd33035872868
Diego Iturra <diturra@dparadig.com>
2023-01-22 19:38:07 -0300

    feat: get image path and title from a book url

--- main.rb
+++ main.rb
@@ -6,11 +6,38 @@ class Scraper
     
     def initialize()
         @list_of_wishlist_urls = [
-            'https://www.buscalibre.cl/v2/pendientes_486712_l.html',
-            'https://www.buscalibre.cl/v2/software-development_503034_l.html',
-            'https://www.buscalibre.cl/v2/comics_545021_l.html'
+            'https://www.buscalibre.cl/v2/pendientes_486712_l.html'
         ]
     end
+
+    def do_scraping_book(url)
+        html = URI.open(url)
+        document = Nokogiri::HTML(html)
+
+        title = get_title(document)
+        image_path = get_image_path(document)
+
+        puts title
+        puts image_path
+
+    end
+
+    def get_title(document)
+        document.css('.tituloProducto').text
+    end
+
+    def get_image_path(document)
+        document.css('.imagen img').find{|picture| picture.attributes["data-src"]}.attributes["data-src"].value
+    end
+
+    def get_price
+        #get book price
+    end
+
+    def get_availability
+        #return availability
+    end
+
 
     def get_all_books_urls
         @list_of_wishlist_urls.each do |url|
@@ -23,7 +50,7 @@ class Scraper
             #Iterate over each div container getting the link to book page
             box_product.css('.box-producto').each do |box|
                 box.css('a').find{ |link| 
-                    puts link.attributes['href'].value
+                    link.attributes['href'].value
                     break
                 }
             end
@@ -31,12 +58,10 @@ class Scraper
     end
 
     def do_scraping
-        get_all_books_urls()
+        get_all_books_urls
     end
-
-
 
 end
 
 scraper = Scraper::new
-scraper.do_scraping
+scraper.do_scraping_book('https://www.buscalibre.cl/libro-transmetropolitan-libro-02-de-5/9788417908928/p/52088837')
