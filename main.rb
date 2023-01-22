05cce254fdfc1e90edce056321632a44c58a89ea
Diego Iturra <diturra@dparadig.com>
2023-01-22 19:13:58 -0300

    feat: scrapping all url from all wishlist

--- main.rb
+++ main.rb
@@ -4,41 +4,37 @@ require 'csv'
 
 class Scraper
     
-    #@@base_url = "https://en.wikipedia.org/wiki/Douglas_Adams"
-
     def initialize()
-        # html = URI.open(@@base_url) #get html file
-        # doc = Nokogiri::HTML(html) #parse html file
-        # description = doc.css("p").text.strip.split("\n")[0] #get first <p> text
-        # url_image = doc.css("img").find{|picture| picture.attributes["alt"].value.include?("Douglas adams portrait cropped.jpg")}.attributes["src"].value
-        # puts url_image
+        @list_of_wishlist_urls = [
+            'https://www.buscalibre.cl/v2/pendientes_486712_l.html',
+            'https://www.buscalibre.cl/v2/software-development_503034_l.html',
+            'https://www.buscalibre.cl/v2/comics_545021_l.html'
+        ]
+    end
 
-        # #Export data to CSV
-        # data_arr = []
-        # data_arr.push(description, url_image)
+    def get_all_books_urls
+        @list_of_wishlist_urls.each do |url|
+            html = URI.open(url)
+            document = Nokogiri::HTML(html)
 
-        # CSV.open('data.csv', "w") do |csv|
-        #     csv << data_arr
-        # end
+            #Get main container for books
+            box_product = document.css('.productos')
+            
+            #Iterate over each div container getting the link to book page
+            box_product.css('.box-producto').each do |box|
+                box.css('a').find{ |link| 
+                    puts link.attributes['href'].value
+                    break
+                }
+            end
+        end
     end
 
     def do_scraping
-        url = 'https://www.buscalibre.cl/v2/feminismo_624086_l.html'
-        html = URI.open(url)
-        document = Nokogiri::HTML(html)
-
-        #Get main container for books
-        box_product = document.css('.productos')
-        
-        #Iterate over each div container getting the link to book page
-        box_product.css('.box-producto').each do |box|
-            box.css('a').find{ |link| 
-                puts link.attributes['href'].value
-                break
-            }
-            puts ""
-        end
+        get_all_books_urls()
     end
+
+
 
 end
 
